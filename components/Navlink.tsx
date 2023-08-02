e546a97a82bde5090333d0e39ff41707c3efe768
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-08-02 21:24:56 +0530

    setting up react router dom and components

---
+++ components/Navlink.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navlink = () => {
+  return (
+    <div>
+      Navlink
+    </div>
+  )
+}
+
+export default Navlink
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { Home, Landing } from "../pages/index";
+
 function App() {
   return (
-    <>
-      <h1>hello World</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/landing" element={<Landing />} />
+      </Routes>
+    </Router>
   );
 }
 
