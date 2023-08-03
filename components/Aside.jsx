f89f09a75213949469c903e266c41fe08288731c
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-08-03 08:48:57 +0530

    aside setup

---
+++ components/Aside.jsx
@@ -0,0 +1,4 @@
+const Aside = () => {
+  return <div className="h-screen bg-blue-50">Aside</div>;
+};
+export default Aside;
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,5 +1,19 @@
+import Aside from "../components/Aside";
+import Navbar from "../components/Navbar";
+import Hero from "./Hero";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main className="h-screen w-screen bg-red-50 grid grid-cols-7">
+      <div>
+        <Aside />
+      </div>
+      <div className="">
+        <Navbar />
+        <Hero />
+      </div>
+    </main>
+  );
 };
 
 export default Home;
