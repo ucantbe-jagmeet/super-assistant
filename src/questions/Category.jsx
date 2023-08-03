75573e0d374fb1784c6b3d76c7705df68fafb6dd
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-08-03 14:29:14 +0530

    drag drop question category done

--- src/questions/Category.jsx
+++ src/questions/Category.jsx
@@ -1,9 +1,95 @@
+/* eslint-disable no-unused-vars */
+import { useState, useRef } from "react";
+
 const Category = () => {
+  const [itemsList, setItemsList] = useState(["item 1", "item 2", "item 3"]);
+  const [ansList, setAnsList] = useState(["ans 1", "ans 2", "ans 3"]);
+
+  // save refernce for dragItem and dragOverItem
+  const dragItem = useRef(null);
+  const dragOverItem = useRef(null);
+
+  // const handle drag sort
+  const handleSort = () => {
+    //duplicate items
+    let newItemsList = [...itemsList];
+
+    // remove and save the dragged item content
+    const draggedItemContent = newItemsList.splice(dragItem.current, 1)[0];
+
+    //switch the position
+    newItemsList.splice(dragOverItem.current, 0, draggedItemContent);
+
+    //reset the position ref
+    dragItem.current = null;
+    dragOverItem.current = null;
+
+    // update the actual array
+    setItemsList(newItemsList);
+  };
+
   return (
-    <div className="w-full h-96 bg-white rounded-md px-10 py-5 border-2 border-grya-400 ">
+    <div className="w-full h-auto bg-white rounded-md px-10 py-5 border-2 border-grya-400 ">
       <h1 className="text-2xl font-semibold text-gray-600">Question 1</h1>
 
       <h2 className="text-gray-500 mt-5"> Categories</h2>
+      <div className="flex justify-center flex-col">
+        {itemsList.map((item, index) => {
+          return (
+            <h2
+              key={index}
+              className="h-9 w-52 bg-red-100 flex items-center pl-5 rounded-sm my-1 cursor-move"
+              draggable
+              onDragStart={(e) => (dragItem.current = index)}
+              onDragEnter={(e) => (dragOverItem.current = index)}
+              onDragEnd={handleSort}
+              onDragOver={(e) => e.preventDefault()}
+            >
+              {item}
+            </h2>
+          );
+        })}
+        <input
+          type="text"
+          id="item"
+          className="h-9 w-52  flex items-center pl-5 rounded-md my-1  text-white border-2"
+          placeholder="Enter category ..."
+          required
+        />
+        <button className="h-10 w-52 bg-blue-400 flex items-center pl-5 rounded-md my-1 font-semibold text-white">
+          + Create More
+        </button>
+      </div>
+
+      {/* items and belong table  */}
+      <div className="h-40 bg-blue-50 mt-3 grid grid-cols-2 px-10">
+        <div>
+          <h2>Item</h2>
+          {ansList.map((item, index) => {
+            return (
+              <h2
+                key={index}
+                className="h-9 w-52 bg-red-100 flex items-center pl-5 rounded-sm my-1 cursor-move"
+              >
+                {item}
+              </h2>
+            );
+          })}
+        </div>
+        <div>
+          <h2>Belongs to</h2>
+          {itemsList.map((item, index) => {
+            return (
+              <h2
+                key={index}
+                className="h-9 w-52 bg-red-100 flex items-center pl-5 rounded-sm my-1 cursor-move"
+              >
+                {item}
+              </h2>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
