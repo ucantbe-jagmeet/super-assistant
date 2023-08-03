2c615adc363caaa0b62aef2a956742e932fdbfdf
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-08-03 08:39:52 +0530

    landing page done

--- pages/Landing.jsx
+++ pages/Landing.jsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <>
+      <div className="w-screen h-screen bg-[--primary-light-blue6] flex items-center justify-center flex-col font-mono ">
+        <h1 className="text-4xl  uppercase font-semibold text-gray-400 md:text-4xl">
+          Super Assistant Developers
+        </h1>
+        <Link to="/">
+          <button className="mt-5 px-5 py-1 bg-[--primary-light-blue4] rounded-md text-white capitalize font-serif tracking-widest">
+            lets go...🚀
+          </button>
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default Landing;
